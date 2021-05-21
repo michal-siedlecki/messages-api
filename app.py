ccf319fc114f029e861cb0f9a825b89a7f856c3b
Michal <siedlecki.michal@gmail.com>
2021-05-21 12:40:29 +0200

    + added basic model class and db config

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
+import os
+
 from flask import Flask
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+ma = Marshmallow()
 
-@app.route("/")
-def index():
-    return "This is yet another version!"
+
+# from models import Result
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,17 @@
+from app import db, ma
+
+class MessageModel(db.Model):
+    __tablename__ = "message_model"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.String(160))
+    views = db.Column(db.Integer, default=1)
+
+
+class MessageModelSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = MessageModel
+        fields = ['id', 'content', 'views']
+
+
+message_schema = MessageModelSchema()
+messages_schema = MessageModelSchema(many=True)
