7b61a8178f7cc4cf810faa4ff95ebcc96d8ecd58
Michal <siedlecki.michal@gmail.com>
2021-05-21 13:16:14 +0200

    modify database uri

--- config.py
+++ config.py
@@ -4,9 +4,10 @@ class Config:
     DEBUG = False
     DEVELOPMENT = False
     SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    if 'postgres' in SQLALCHEMY_DATABASE_URI:
-        SQLALCHEMY_DATABASE_URI.replace('postgres', 'postgresql')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
 
 class ProductionConfig(Config):
     pass
